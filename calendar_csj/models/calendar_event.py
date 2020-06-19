f2c74198947905eaf3e9b1c8c850b1fedc099f4f
Oscar B <ob@todoo.co>
2020-06-18 23:17:10 -0500

    Test2 [FIX] calendar_csj: get ics file function in event (calendar_csj module) from Meeting (odoo addons calendar module) comment

--- calendar_csj/models/calendar_event.py
+++ calendar_csj/models/calendar_event.py
@@ -101,3 +101,63 @@ class CalendarEvent(models.Model):
                 attendee_to_email = record.attendee_ids
                 if attendee_to_email:
                     attendee_to_email._send_mail_to_attendees('calendar_csj.calendar_csj_template_meeting_cancel')
+
+    # def _get_ics_file(self):
+    #     """ Returns iCalendar file for the event invitation.
+    #         :returns a dict of .ics file content for each meeting
+    #     """
+    #     result = {}
+
+    #     def ics_datetime(idate, allday=False):
+    #         if idate:
+    #             if allday:
+    #                 return idate
+    #             else:
+    #                 return idate.replace(tzinfo=pytz.timezone('UTC'))
+    #         return False
+
+    #     try:
+    #         # FIXME: why isn't this in CalDAV?
+    #         import vobject
+    #     except ImportError:
+    #         _logger.warning("The `vobject` Python module is not installed, so iCal file generation is unavailable. Please install the `vobject` Python module")
+    #         return result
+
+    #     for meeting in self:
+    #         cal = vobject.iCalendar()
+    #         event = cal.add('vevent')
+
+    #         if not meeting.start or not meeting.stop:
+    #             raise UserError(_("First you have to specify the date of the invitation."))
+    #         event.add('created').value = ics_datetime(fields.Datetime.now())
+    #         event.add('dtstart').value = ics_datetime(meeting.start, meeting.allday)
+    #         event.add('dtend').value = ics_datetime(meeting.stop, meeting.allday)
+    #         event.add('summary').value = meeting.name
+    #         if meeting.description:
+    #             event.add('description').value = meeting.description
+    #         if meeting.location:
+    #             event.add('location').value = meeting.location
+    #         if meeting.rrule:
+    #             event.add('rrule').value = meeting.rrule
+
+    #         if meeting.alarm_ids:
+    #             for alarm in meeting.alarm_ids:
+    #                 valarm = event.add('valarm')
+    #                 interval = alarm.interval
+    #                 duration = alarm.duration
+    #                 trigger = valarm.add('TRIGGER')
+    #                 trigger.params['related'] = ["START"]
+    #                 if interval == 'days':
+    #                     delta = timedelta(days=duration)
+    #                 elif interval == 'hours':
+    #                     delta = timedelta(hours=duration)
+    #                 elif interval == 'minutes':
+    #                     delta = timedelta(minutes=duration)
+    #                 trigger.value = delta
+    #                 valarm.add('DESCRIPTION').value = alarm.name or u'Odoo'
+    #         for attendee in meeting.attendee_ids:
+    #             attendee_add = event.add('attendee')
+    #             attendee_add.value = u'MAILTO:' + (attendee.email or u'')
+    #         result[meeting.id] = cal.serialize().encode('utf-8')
+
+    #     return result
