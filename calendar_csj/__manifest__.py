7a1f9ff21db8edae8bbcb106cd3d902368839c78
Jhair Escobar <je@todoo.co>
2020-07-08 09:46:58 -0500

    master [FIX] calendar_csj: changes in smtp email indentification in ics file generation

--- calendar_csj/__manifest__.py
+++ calendar_csj/__manifest__.py
@@ -14,7 +14,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Marketing/Online Appointment',
-    'version': '13.1.2',
+    'version': '13.1',
 
     # any module necessary for this one to work correctly
     'depends': ['website_calendar','contacts','base_address_city','event', 'calendar'],
