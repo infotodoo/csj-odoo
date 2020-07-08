a3c284ca44e3732b53e831e33e517cc381fedc13
Jhair Escobar <je@todoo.co>
2020-07-08 09:29:44 -0500

    master [FIX] calendar_csj: changes in smtp email indentification in ics file generation

--- calendar_csj/models/event.py
+++ calendar_csj/models/event.py
@@ -149,10 +149,10 @@ class Meeting(models.Model):
                 event.add('rrule').value = meeting.rrule
 
             organizer_add = event.add('organizer')
-            organizer_add.params['CN'] = ["agendamiento.csj@gmail.com"]
+            organizer_add.params['CN'] = ["agendamiento@csj.life"]
             organizer_add.params['ROLE'] = ["CHAIR"]
             organizer_add.params['RSVP'] = ["TRUE"]
-            organizer_add.value = "agendamiento.csj@gmail.com"
+            organizer_add.value = "agendamiento@csj.life"
 
             if meeting.alarm_ids:
                 for alarm in meeting.alarm_ids:
