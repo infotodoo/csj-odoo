4736fb229215f81916797c2c60125ab486be7882
Oscar B <ob@todoo.co>
2020-07-30 01:40:20 -0500

    Test2 [FIX] calendar_csj: Change logic for moderator extension

--- calendar_csj/models/calendar_appointment.py
+++ calendar_csj/models/calendar_appointment.py
@@ -328,10 +328,9 @@ class CalendarAppointment(models.Model):
             api.update({
                 'ownerExtension': self.env.user.extension_lifesize or \
                     self.env.user.company_id.owner_extension,
-                'moderatorExtension': self.env.user.extension_lifesize or \
+                'moderatorExtension': judged_extension_lifesize or \
+                    self.env.user.extension_lifesize or \
                     self.env.user.company_id.owner_extension,
-                # 'moderatorExtension': judged_extension_lifesize or \
-                #     self.env.user.company_id.owner_extension,
             })
         else:
             api.update({
