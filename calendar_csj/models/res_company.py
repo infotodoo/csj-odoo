425025e63ca7b824ce77e27f2487e003b2cba877
Oscar B <ob@todoo.co>
2020-08-13 17:28:56 -0500

    Test2 [FIX] calendar_csj: add logger to ResUsers

--- calendar_csj/models/res_company.py
+++ calendar_csj/models/res_company.py
@@ -1,19 +1,30 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api, _
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 class ResCompany(models.Model):
-    _inherit = 'res.company'
+    _inherit = "res.company"
 
-    key_lifesize = fields.Char('Token lifesize')
-    owner_extension = fields.Char('Owner extension')
-    lecturer_extension = fields.Char('Lecturer extension')
-    moderator_extension = fields.Char('Moderator extension')
+    key_lifesize = fields.Char("Token lifesize")
+    owner_extension = fields.Char("Owner extension")
+    lecturer_extension = fields.Char("Lecturer extension")
+    moderator_extension = fields.Char("Moderator extension")
 
 
 class ResUsers(models.Model):
-    _inherit = 'res.users'
+    _inherit = "res.users"
 
-    extension_lifesize = fields.Char('Extension Lifesize')
-    notification_partner = fields.Many2one('res.partner', 'Notification Partner')
+    extension_lifesize = fields.Char("Extension Lifesize")
+    notification_partner = fields.Many2one("res.partner", "Notification Partner")
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        users = super(ResUsers, self).create(vals_list)
+        _logger.error(
+            f"\nCREATE USER:\nusers_object: {users}\nvals_list: {vals_list}\n"
+        )
+        return users
