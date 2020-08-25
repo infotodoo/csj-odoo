f4e8cef6ac2457a5ce4ba9eec7eb84ee6cb2977b
Jhair Escobar <je@todoo.co>
2020-08-24 23:50:57 -0500

    master [ADD] calendar_csj: Version 2.0 first release in production enviroment RC2.1.3

--- calendar_csj/models/__init__.py
+++ calendar_csj/models/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from . import api_lifesize
-#from . import res_company
+from . import res_company
 from . import res_partner
 from . import calendar_appointment
 from . import calendar_event
