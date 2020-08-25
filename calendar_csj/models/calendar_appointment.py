753d5e5e2cc9a2bd10180a6d0b591cbb221f368d
nf-todoo <62726399+nf-todoo@users.noreply.github.com>
2020-08-24 20:20:12 -0500

    Update calendar_appointment.py

--- calendar_csj/models/calendar_appointment.py
+++ calendar_csj/models/calendar_appointment.py
@@ -79,7 +79,7 @@ class CalendarAppointment(models.Model):
                               'State', default='draft', tracking=True)
     # Realizada, Duplicada, No realizada, Asistida aplazada, Asistida cancelada, Cancelada
     
-    state_label = fields.Char(string='Estado en español', compute='_get_state_label')
+    state_label = fields.Char(string='Estado en español')
     
     name = fields.Char('Name', default=_('New'))
     active = fields.Boolean('Active', default=True)
