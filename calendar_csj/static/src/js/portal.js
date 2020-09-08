c9dcf436826c36df9efe3a6802d49a3d0790847e
nf-todoo <62726399+nf-todoo@users.noreply.github.com>
2020-09-08 15:07:34 -0500

    Update portal.js

--- calendar_csj/static/src/js/portal.js
+++ calendar_csj/static/src/js/portal.js
@@ -267,7 +267,7 @@ odoo.define('calendar_csj.calendar_portal_csj', function(require) {
     $("#state_draft_btn").on('click', function(e){
       Dialog.confirm(this, 'El agendamiento será marcado como Duplicado', {
           confirm_callback: function() {
-            var url = '/my/appointment/' + appointment_id + '/update/state';
+            var url = '/my/appointment/' + appointment_id + '/update/state/postpone';
             window.location.href = url;
           },
           title: _t('Confirmación para posponer el Agendamiento'),
