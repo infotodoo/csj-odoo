c7c65f713bac94f7df9eb389d818e1986c0fd23e
nf-todoo <62726399+nf-todoo@users.noreply.github.com>
2020-09-09 17:05:57 -0500

    Update portal.py

--- calendar_csj/controllers/portal.py
+++ calendar_csj/controllers/portal.py
@@ -496,7 +496,7 @@ class CustomerPortal(CustomerPortal):
     @http.route(['/my/appointment/<model("calendar.appointment"):appointment_id>/update/state/draft'], type='http', auth="user", website=True)
     def portal_my_appointment_assist_draft(self, appointment_id=None, access_token=None, **kw):
         appointment_id.write({
-            'state' : 'draft',
+            'state' : 'open',
             'appointment_close_date': datetime.now(),
             'appointment_close_user_id': request.env.user.id,
         });
