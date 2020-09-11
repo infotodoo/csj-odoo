bf913d30d116a9be17e25bd87762e872b3821095
nf-todoo <nf@todoo.co>
2020-09-10 21:56:22 -0500

    Test2 [ADD] calendar_csj: Adicionando boton actualizar frontend

--- calendar_csj/models/calendar_appointment.py
+++ calendar_csj/models/calendar_appointment.py
@@ -104,7 +104,7 @@ class CalendarAppointment(models.Model):
     partaker_type = fields.Many2one('calendar.help', 'Portaker Type', ondelete='set null')  # Ayuda
     connection_type = fields.Many2one('calendar.help', 'Connection Type', ondelete='set null')  # Ayuda
     request_date = fields.Date('Request date')  # Date
-    appointment_date = fields.Date('Appointment date', default=fields.Date.today(), compute='_get_date_today')  # Date
+    appointment_date = fields.Date('Appointment date', default=fields.datetime.now(pytz.timezone("America/Bogota")))  # Date
     appointment_close_date = fields.Date('Close date')
     appointment_close_user_id = fields.Many2one('res.users', 'Close user')  # Date
     appointment_close_user_login = fields.Char('Closing User Login', related='appointment_close_user_id.login', store=False)  # Date
@@ -275,11 +275,11 @@ class CalendarAppointment(models.Model):
                 record.calendar_datetime.minute/60.0 if \
                 record.calendar_datetime else False 
 
-    @api.depends('appointment_date')
-    def _get_date_today(self):
-        for record in self:
-            if record.appointment_date:
-                record.appointment_date = (record.appointment_date - datetime.timedelta(hours=5)) 
+    # @api.depends('appointment_date')
+    # def _get_date_today(self):
+    #     for record in self:
+    #         if record.appointment_date:
+    #             record.appointment_date = (record.appointment_date - datetime.timedelta(1)) 
 
 
     @api.depends('applicant_id')
