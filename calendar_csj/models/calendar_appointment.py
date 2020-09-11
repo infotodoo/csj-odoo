c753696c088225e947db542c059750185569daeb
nf-todoo <nf@todoo.co>
2020-09-10 21:43:48 -0500

    Test2 [ADD] calendar_csj: Adicionando boton actualizar frontend

--- calendar_csj/models/calendar_appointment.py
+++ calendar_csj/models/calendar_appointment.py
@@ -278,7 +278,10 @@ class CalendarAppointment(models.Model):
     @api.depends('appointment_date')
     def _get_date_today(self):
         for record in self:
-            record.appointment_date = (record.appointment_date - datetime.timedelta(hours=5)).date() 
+            if record.appointment_date:
+                record.appointment_date = (record.appointment_date - datetime.timedelta(hours=5)).date() 
+            else:
+                record.appointment_date = False
 
     @api.depends('applicant_id')
     def _compute_applicant_id(self):
