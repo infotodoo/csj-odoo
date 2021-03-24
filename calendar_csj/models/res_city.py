466c90d949e504b38a31e2993cd9cc702a12e35f
Jhair Escobar <je@todoo.co>
2021-03-23 20:49:15 -0500

    [FIX] calendar_csj: Actualización Endpoint Usuarios

--- calendar_csj/models/res_city.py
+++ calendar_csj/models/res_city.py
@@ -8,9 +8,15 @@ _logger = logging.getLogger(__name__)
 
 class ResCity(models.Model):
     _inherit = "res.city"
-
+    
     def name_get(self):
         result = []
         for rec in self:
             result.append((rec.id, rec.name + ' - ' + rec.state_id.name + ' - ' + rec.zipcode))
         return result
+
+
+class ResCountryState(models.Model):
+    _inherit = "res.country.state"
+    
+    dane_code = fields.Char('Código Dane')
