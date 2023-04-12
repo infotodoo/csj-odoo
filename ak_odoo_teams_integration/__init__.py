85df27d09ffb0ee73e13dacffe61f5e4c531533e
Jhair Escobar <je@todoo.co>
2023-04-11 23:31:06 -0500

    [ADD] calendar_csj: add teams api

--- ak_odoo_teams_integration/__init__.py
+++
@@ -1,4 +0,0 @@
-# Part of Odoo, Aktiv Software.
-# See LICENSE file for full copyright & licensing details.
-from . import models
-from . import controllers
--- ak_odoo_teams_integration/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# Part of Odoo, Aktiv Software.
-# See LICENSE file for full copyright & licensing details.
-from . import calendar_event
-from . import res_company
-from . import res_users
